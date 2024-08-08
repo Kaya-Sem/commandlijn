4b76d7bda9fab09c2ef5e09926cf1780cbc67f85
Kaya-Sem <kayasemvc@gmail.com>
2024-08-08 21:18:24 +0200

    added print for transitpoints

--- cmd/util.go
+++ cmd/util.go
@@ -1,7 +1,17 @@
 package cmd
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func replaceSpacesWithURLCode(input string) string {
 	return strings.ReplaceAll(input, " ", "%20")
+}
+
+func printTransitPoints(tp []TransitPoint) {
+	for _, t := range tp {
+		fmt.Printf("Entiteitnummer: %s\nOmschrijving: %s\nHaltenummer: %s\nProvider: %s\n\n",
+			t.Entiteitnummer, t.Omschrijving, t.Haltenummer, t.TransitProvider)
+	}
 }
