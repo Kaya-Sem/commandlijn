4fdedfc95a433096b579a378c3f5f5ddf01a5416
Kaya-Sem <kayasemvc@gmail.com>
2024-08-10 17:28:23 +0200

    added versioning to sourcecode

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,43 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+var Version string // This will be set from main.go
+
 var (
 	verbose bool
 	rootCmd = &cobra.Command{
 		Use:   "commandlijn",
 		Short: "A brief description of your application",
 		Long:  ``,
+		// Define the default action when no subcommands are provided
+		Run: func(cmd *cobra.Command, args []string) {
+			if v, _ := cmd.Flags().GetBool("version"); v {
+				fmt.Println("Version:", Version)
+				os.Exit(0)
+			}
+			fmt.Println("Use --help to see available commands.")
+		},
 	}
 )
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
+	// Define the --version flag
+	rootCmd.Flags().BoolP("version", "V", false, "Print the version number and exit")
 
+	// Define other flags
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/Kaya-Sem/commandlijn/cmd"
 
+const Version = "0.0.0"
+
 func main() {
+	cmd.Version = Version
 	cmd.Execute()
 }
