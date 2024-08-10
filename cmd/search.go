6f7ed17256bb650c90c121bffab77961a746670f
Kaya-Sem <kayasemvc@gmail.com>
2024-08-10 10:40:48 +0200

    added subcommands for searching
    - delijn for searching delijn haltes
    - sncb for searching SNCB haltes

--- cmd/search.go
+++ cmd/search.go
@@ -15,20 +15,24 @@ var limit int
 
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
-	Use:   "search [searchterm]",
+	Use:   "search",
 	Short: "Search for public transport stops.",
 	Long: `Search for public transport stops using a search term. 
-The term can be a name of a place or stop. Searches for both De Lijn and SNCB`,
-	Args: cobra.ExactArgs(1), // Ensure exactly one argument is provided
+The term can be a name of a place or stop. Searches for both De Lijn and SNCB.`,
+}
+
+// delijnCmd represents the delijn subcommand
+var delijnCmd = &cobra.Command{
+	Use:   "delijn [searchterm]",
+	Short: "Search for De Lijn public transport stops.",
+	Long:  `Search for De Lijn public transport stops using a search term. The term can be a name of a place or stop.`,
+	Args:  cobra.ExactArgs(1), // Ensure exactly one argument is provided
 	Run: func(cmd *cobra.Command, args []string) {
 		searchterm := args[0] // Get the search term from the arguments
-		trainSet := []string{"🚅", "🚆", "🚈", "🚌"}
-		s := spinner.New(trainSet, 100*time.Millisecond)
-		//s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+		s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 		s.Start()
 		defer s.Stop()
 
-		// TODO: also search SNCB stops
 		haltesJson := apiHalteSearch(searchterm, limit)
 		s.Stop()
 		if haltesJson != nil {
@@ -43,8 +47,41 @@ The term can be a name of a place or stop. Searches for both De Lijn and SNCB`,
 	},
 }
 
+// sncbCmd represents the sncb/nmbs subcommand
+var sncbCmd = &cobra.Command{
+	Use:   "sncb [searchterm]",
+	Short: "Search for SNCB/NMBS public transport stops.",
+	Long:  `Search for SNCB/NMBS public transport stops using a search term. The term can be a name of a place or stop.`,
+	Args:  cobra.ExactArgs(1), // Ensure exactly one argument is provided
+	Run: func(cmd *cobra.Command, args []string) {
+		searchterm := args[0] // Get the search term from the arguments
+		s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+		s.Start()
+		defer s.Stop()
+
+		// TODO: Implement SNCB/NMBS search logic
+		fmt.Printf("todo implement searchterm filtering: %s", searchterm)
+		sncbJson := getSNCBStationsJSON()
+		s.Stop()
+		if sncbJson != nil {
+			transitPoints, err := parseiRailTransitPoints(sncbJson)
+			if err != nil {
+				fmt.Println("Error parsing JSON:", err)
+				return
+			}
+
+			printTransitPoints(transitPoints)
+		}
+	},
+}
+
 func init() {
 	// Define the flag for limit
 	searchCmd.Flags().IntVarP(&limit, "limit", "l", SearchLimit, "Limit the number of results")
+
+	// Add subcommands to the search command
+	searchCmd.AddCommand(delijnCmd)
+	searchCmd.AddCommand(sncbCmd)
+
 	rootCmd.AddCommand(searchCmd)
 }
