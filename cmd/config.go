17438a063061597db8fe0a59dea5d05c034ad13a
Kaya-Sem <kayasemvc@gmail.com>
2024-08-11 18:08:46 +0200

    added method to retrieve config struct from config file

--- cmd/config.go
+++ cmd/config.go
@@ -1,5 +1,12 @@
 package cmd
 
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 type Config struct {
 	DeLijnAPIKey string  `yaml:"delijn_api_key"`
 	Aliases      []Alias `yaml:"aliases"`
@@ -9,4 +16,22 @@ type Alias struct {
 	Name     string          `yaml:"name"`
 	Provider TransitProvider `yaml:"provider"`
 	ID       []string        `yaml:"ID"`
+}
+
+func getConfig() Config {
+	data, err := os.ReadFile(getConfigFilePath())
+	if err != nil {
+		log.Printf("Error getting config file at %s: %v", getConfigFilePath(), err)
+		os.Exit(ErrFileRead)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Printf("Error parsing config: %v", err)
+		os.Exit(ErrUnmarshal)
+	}
+
+	return config
 }
